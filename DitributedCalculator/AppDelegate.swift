4d3d2a760f1c6c97585f2e0a27e6c4fba56e0fe5
Илья Халяпин <962426@mail.ru>
2018-02-23 17:15:29 +0300

    Initial Commit

---
+++ DitributedCalculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DitributedCalculator
+//
+//  Created by Илья Халяпин on 23.02.2018.
+//  Copyright © 2018 Ilia Khaliapin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DitributedCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DitributedCalculator
+//
+//  Created by Илья Халяпин on 23.02.2018.
+//  Copyright © 2018 Ilia Khaliapin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
