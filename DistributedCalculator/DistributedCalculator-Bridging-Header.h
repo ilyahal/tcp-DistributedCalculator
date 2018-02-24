b179a96087019de34b137cde208ca34fe8cda682
Илья Халяпин <962426@mail.ru>
2018-02-24 14:33:08 +0300

    add receiving ip address

---
+++ DistributedCalculator/DistributedCalculator-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  DistributedCalculator-Bridging-Header.h
+//  DistributedCalculator
+//
+//  Created by Илья Халяпин on 24.02.2018.
+//  Copyright © 2018 Ilia Khaliapin. All rights reserved.
+//
+
+#include <ifaddrs.h>
--- DistributedCalculator/ViewController.swift
+++ DistributedCalculator/ViewController.swift
@@ -20,7 +20,17 @@ class ViewController: NSViewController {
     @IBOutlet private weak var listFilePathControl: NSPathControl!
     
     /// IP сервера
-    @IBOutlet private weak var serverIpTextField: NSTextField!
+    @IBOutlet private weak var serverIpTextField: NSTextField! {
+        willSet {
+            DispatchQueue.global().async { [weak self, newValue] in
+                let ipAddress = self?.getWiFiAddress()
+                
+                DispatchQueue.main.async {
+                    newValue?.stringValue = ipAddress ?? ""
+                }
+            }
+        }
+    }
     
     /// Кнопка "Запустить сервер"
     @IBOutlet private weak var serverStartButton: NSButton!
@@ -180,6 +190,39 @@ private extension ViewController {
 // MARK: - Приватные методы
 
 private extension ViewController {
+    
+    /// Получение IP-адреса
+    func getWiFiAddress() -> String? {
+        var address: String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        guard let firstAddr = ifaddr else { return nil }
+        
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            // Check for IPv4 or IPv6 interface:
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if name == "en0" {
+                    
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        
+        return address
+    }
     
     /// Лог
     func log(message: String) {
